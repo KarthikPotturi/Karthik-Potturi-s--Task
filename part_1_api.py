31bfe421a7867279b165f0aa7de37360e8d93b45
KarthikPotturi <karthikpotturi@gmail.com>
2023-09-30 08:23:35 +0800

    Added comments to part_1_api.py

--- part_1_api.py
+++ part_1_api.py
@@ -2,6 +2,7 @@ import requests, time
 import pandas as pd
 
 def fetch_all_records(api_url, max_retries = 3, retry_delay = 1):
+    # Set the user agent for the request
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
     records = []
     offset = 0
@@ -14,8 +15,11 @@ def fetch_all_records(api_url, max_retries = 3, retry_delay = 1):
             'offset':offset,
             'sort': 'end_of_week desc'
         }
+        
+        # Make a GET request to the API
         response = requests.get(api_url, headers=headers, params=params)
         if response.status_code == 200:
+            # Successful response, parse and collect records
             data = response.json()
             records.extend(data['result']['records'])
             print(len(data['result']['records']))
@@ -23,6 +27,7 @@ def fetch_all_records(api_url, max_retries = 3, retry_delay = 1):
                 break
             offset = offset + limit
         else:
+            # Handle unsuccessful response
             print("Failed to fetch the data. Status Code {}".format(response.status_code))
             if retries < max_retries:
                 retries += 1
@@ -34,9 +39,13 @@ def fetch_all_records(api_url, max_retries = 3, retry_delay = 1):
     return records
 
 if __name__ == '__main__':
+    # API endpoint
     api_url = "https://eservices.mas.gov.sg/api/action/datastore/search.json"
+    
+    # Fetch all records from the API
     all_records = fetch_all_records(api_url)
 
+    # Convert records to a DataFrame and save to CSV
     data_dict = {record['end_of_week']:record for record in all_records}
     #print("Total records {}".format(len(data_dict)))
     df = pd.DataFrame.from_dict(data_dict)
